d6471b6a1d42a85a6e74e497dba445e80e7a0654
Soloman Garcia <soloman.garcia@gmail.com>
2022-09-06 10:15:21 -0600

    work on slider js

---
+++ js/app.js
@@ -0,0 +1,41 @@
+let init = () => {
+  let container = document.getElementsByClassName("jumbo-slider__container")[0],
+    slides = document.getElementsByClassName("jumbo-slider__slide"),
+    circles = document.getElementsByClassName("jumbo-slider__circle"),
+    links = document.getElementsByClassName("jumbo-slider__link"),
+    current = 1,
+    time = 6000;
+
+  // add animation class to slide
+  slides[0].classList.add("jumbo-slider__slide--active");
+  links[current - 1].classList.add("jumbo-slider__link--active");
+
+  // update ellipsis and links
+  let updateNav = (current) => {
+    for (let i = 0; i < slides.length; i++) {
+      links[i].classList.remove("jumbo-slider__link--active");
+      circles[i].classList.remove("jumbo-slider__circle--filled");
+    }
+    links[current - 1].classList.add("jumbo-slider__link--active");
+    circles[current - 1].classList.add("jumbo-slider__circle--filled");
+  };
+
+  let startSliding = () => {
+    setInterval(() => {
+      slides[1].classList.add("jumbo-slider__slide--active");
+      slides[0].classList.remove("jumbo-slider__slide--active");
+
+      if (current < slides.length) {
+        current++;
+        updateNav(current);
+      } else {
+        current = 1;
+      }
+      container.appendChild(slides[0].cloneNode([true]));
+      container.removeChild(slides[0]);
+    }, 6000);
+  };
+  startSliding();
+};
+
+init();
